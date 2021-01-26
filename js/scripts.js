c49fe8f0b47fa26a62afca75a1299f415a8c2716
Marguerite Kenedy <reach.marguerite@gmail.com>
2021-01-26 14:58:57 -0800

    move JQuery cdn to head

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+    // popup opener
+    
+    
+    $(function() {
+  
+        // contact form popup JS //
+        $('#contact-pop').click(function() {
+          $('#contactForm').fadeToggle();
+        })
+        $(document).mouseup(function (e) {
+          var container = $("#contactForm");
+      
+          if (!container.is(e.target) // if the target of the click isn't the container...
+              && container.has(e.target).length === 0) // ... nor a descendant of the container
+          {
+              container.fadeOut();
+          }
+        });
+        
+      });
